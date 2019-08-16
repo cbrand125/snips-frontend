61123a9b55b00bd517d8e1a8cc95a5b4c1c87653
cbrand125 <cbrand@talentpath.com>
2019-08-16 08:22:20 -0700

    add snippet search

--- index.js
+++ index.js
@@ -1,30 +1,6 @@
 import SNIPS from './snippet-data.js';
-
-// map over snips
-// transform snip into html
-// profit
-// put html into #snippets
-
-const renderSnips = snippets => {
-  const snippetHTML = snippets
-    .map(
-      snippet => /* html */ `
-      <div class="snip">
-        <div class="text">
-          <h2>${snippet.title}</h2>
-          <p>${snippet.description}</p>
-        </div>
-        <pre><code class="${snippet.language}">${he.encode(
-        snippet.code
-      )}</code></pre>
-      </div>
-    `
-    )
-    .join('');
-
-  const snippetElement = document.getElementById('snippets');
-  snippetElement.innerHTML = snippetHTML;
-};
+import { renderSnips } from './lib/snippets.js';
+import search from './lib/search.js';
 
 renderSnips(SNIPS);
 hljs.initHighlightingOnLoad();
